315e20ec196dc769da029fcecea069808c8e8f64
practica <practica>
2016-02-26 00:46:58 +0100

    Some fixing and including mortal-victims. Closes #11. Closes #12.

--- about.js
+++ about.js
@@ -2,7 +2,7 @@ var express= require("express");
 
 var app = express();
 
-app.get("/", (req,res) => {
+app.get("/about", (req,res) => {
 
 res.write("<html><body><div>Welcome, this is an app from group 10 of SOS.
  We are working with data related to gender-violence</div><ul>
