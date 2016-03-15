3b17cfaf0813b24d7fdfe38013e6f17cd9562f8a
Pascual Requena Lama <reque1993@gmail.com>
2016-03-15 13:43:34 +0100

    finally done v2

--- index.js
+++ index.js
@@ -157,105 +157,95 @@ app.get("/api-test/teams/loadInitialData", (req,res)=>{         //
                                                                 //
 //////////////////////////////////////////////////////////////////
 
-// /api/sandbox/videogames ////////////////////////////////////////////
-//var contacts = [{ name : "pepe"}];                            //
+/////////////////////// /api/sandbox/videogames //////////////////////////
 var videogames = [{ name: "LeagueOflegends", platform: "Computer"},
-{ name: "BloodBorn", platform: "Playstation"}]; //No es JSON(es JavaScript)  //
-                                                                //
-//Función para buscar en array "videojuegos" en mi caso               //                                                        //
-                                                                //
-app.get("/api/sandbox/videogames", (req,res) => {                    //
-  console.log("New GET of videogames");                              //
-  res.send(videogames);                                              //
-});                                                             //
-app.post("/api/sandbox/videogames", (req,res) => {                   //
-  var vg = req.body;                                          //
-                                                                //
-  //comprobar antes que no existe ese "name" ya                 //
-  var e = find_resource(videogames,vg.name)[0];                    //
-  if(e == 0){ //No hay error(lo encuentra, ya existe)           //
-    res.sendStatus(409); //Conflict                             //
-    console.log("NOT POST because \""+vg.name+"\" exist");    //
-  } else {                                                      //
-    teams.push(vg);                                           //
-    console.log("New POST of resource "+vg.name);             //
-    res.sendStatus(200);                                        //
-  }                                                             //
-});                                                             //
-//NO PERMITIDO                                                  //
-app.put("/api/sandbox/videogames", (req,res) => {                    //
-  console.log("PUT NOT ALLOWED");                               //
-  res.sendStatus(405);                                          //
-});                                                             //
-app.delete("/api/sandbox/videogames", (req,res) => {                 //
-  console.log("New DELETE of TEAMS");                           //
-  videogames.splice(0,videogames.length);                                 //
-  res.sendStatus(200);                                          //
-});                                                             //
-                                                                //
-// /api/sandbox/teams/betis //////////////////////////////////////
-app.get("/api/sandbox/videogames/:name", (req,res)=>{                //
-  var n = req.params.name;                                      //
-  console.log("New GET of resource "+n);                        //
-                                                                //
-  var e = find_resource(videogames,n)[0];                            //
-  var i = find_resource(videogames,n)[1];                            //
-  if(e == 0){ //Error == 0 "NO hay error"                       //
-    res.send(videogames[i]);                                         //
-  } else {                                                      //
-    res.sendStatus(404);                                        //
-  }                                                             //
-});                                                             //
-//NO PERMITIDO                                                  //
-app.post("/api/sandbox/videogames/:name", (req,res) => {             //
-  console.log("POST NOT ALLOWED");                              //
-  res.sendStatus(405);                                          //
-});                                                             //
-app.put("/api/sandbox/videogames/:name", (req,res) => {              //
-  //var n = req.params.name;                                    //
-  var n = req.body.name;                                        //
-                                                                //
-  var e = find_resource(videogames,n)[0];                            //
-  var i = find_resource(videogames,n)[1];                            //
-  if(e == 0){ //No hay error(lo encuentra, ya existe)           //
-    videogames.splice(i, 1); //Elimino objeto                        //
-    videogames.push(req.body); //Añado objeto                        //
-    console.log("New PUT of resource "+n);                      //
-    res.sendStatus(200);                                        //
-  } else {                                                      //
-    console.log("Resource \""+n+"\" NOT exist");                //
-    res.sendStatus(404);                                        //
-  }                                                             //
-});                                                             //
-app.delete("/api/sandbox/videogames/:name", (req,res) => {           //
-  var n = req.params.name;                                      //
-                                                                //
-  var e = find_resource(videogames,n)[0];                            //
-  var i = find_resource(videogames,n)[1];                            //
-  if(e == 0){ //Lo encuentro en "teams"                         //
-    videogames.splice(i, 1); //delete teams[i];                      //
-    console.log("New DELETE of resource "+n);                   //
-    res.sendStatus(200);                                        //
-  } else { //No lo encuentro en "teams"                         //
-    console.log("Not DELETE because NOT FOUND "+n);             //
-    res.sendStatus(404);                                        //
-  }                                                             //
-});                                                             //
-//Para inicializar la API REST "teams" ///////////////////////////
-// /api-test/XXXXX/loadInitialData“                             //
-app.get("/api-test/videogames/loadInitialData", (req,res)=>{         //
-  console.log("/api-test/videogames/loadInitialData");               //
+{ name: "BloodBorn", platform: "Playstation"}];
+
+app.get("/api/sandbox/videogames", (req,res) => {
+  console.log("New GET of videogames");
+  res.send(videogames);
+});
+app.post("/api/sandbox/videogames", (req,res) => {
+  var vg = req.body;
+
+
+  var e = find_resource(videogames,vg.name)[0];
+  if(e == 0){
+    res.sendStatus(409);
+    console.log("NOT POST because \""+vg.name+"\" exist");
+  } else {
+    teams.push(vg);
+    console.log("New POST of resource "+vg.name);
+    res.sendStatus(200);
+  }
+});
+//NO PERMITIDO
+app.put("/api/sandbox/videogames", (req,res) => {
+  console.log("PUT NOT ALLOWED");
+  res.sendStatus(405);
+});
+app.delete("/api/sandbox/videogames", (req,res) => {
+  console.log("New DELETE of TEAMS");
+  videogames.splice(0,videogames.length);
+  res.sendStatus(200);
+});
+
+
+app.get("/api/sandbox/videogames/:name", (req,res)=>{
+  var n = req.params.name;
+  console.log("New GET of resource "+n);
+  var e = find_resource(videogames,n)[0];
+  var i = find_resource(videogames,n)[1];
+  if(e == 0){
+    res.send(videogames[i]);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.post("/api/sandbox/videogames/:name", (req,res) => {
+  console.log("POST NOT ALLOWED");
+  res.sendStatus(405);
+});
+
+app.put("/api/sandbox/videogames/:name", (req,res) => {
+  var n = req.body.name;
+  var e = find_resource(videogames,n)[0];
+  var i = find_resource(videogames,n)[1];
+  if(e == 0){
+    videogames.splice(i, 1);
+    videogames.push(req.body);
+    console.log("New PUT of resource "+n);
+    res.sendStatus(200);
+  } else {
+    console.log("Resource \""+n+"\" NOT exist");
+    res.sendStatus(404);
+  }
+});
+app.delete("/api/sandbox/videogames/:name", (req,res) => {
+  var n = req.params.name;
+  var e = find_resource(videogames,n)[0];
+  var i = find_resource(videogames,n)[1];
+  if(e == 0){
+    videogames.splice(i, 1);
+    console.log("New DELETE of resource "+n);
+    res.sendStatus(200);
+  } else {
+    console.log("Not DELETE because NOT FOUND "+n);
+    res.sendStatus(404);
+  }
+});
+
+app.get("/api-test/videogames/loadInitialData", (req,res)=>{
+  console.log("/api-test/videogames/loadInitialData");
   videogames = [
         { name: "LeagueOfLegends", platform: "Computer"},
         { name: "BloodBorn", platform: "PlayStation"},
-        { name: "Halo", platform: "Xbox"},
-        { name: "WorldOfWarcraft", platform: "Computer"}
-  ];                                                            //
-  res.send(videogames);                                              //
-  //res.sendStatus(200);                                        //
-});                                                             //
-                                                                //
-//////////////////////////////////////////////////////////////////
+        { name: "Halo", platform: "Xbox"}
+  ];
+  res.send(videogames);
+});
+
 
 
 //app.listen(3000); //Para probar en local
