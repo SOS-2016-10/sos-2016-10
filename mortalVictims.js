07a553334cee83919270073851b8a7759ade019e
Pascual Requena Lama <reque1993@gmail.com>
2016-03-29 01:13:46 +0200

    Fixing bugs v2.0

--- mortalVictims.js
+++ mortalVictims.js
@@ -103,7 +103,6 @@ module.exports.deleteVictim = (req,res)=>{
 // /api/v1/mortal-victims/loadInitialData“
 module.exports.loadInitialData = (req,res)=>{
   console.log("/api/v1/mortal-victims/loadInitialData");
-  var victims = [
     var victims = [{autonomous_community: "Andalucia", year: 2015, under18age: 0, from18to40age: 3, over40age: 7},
                     {autonomous_community: "Madrid", year: 2013, under18age: 0, from18to40age: 5, over40age: 4},
                     {autonomous_community: "Cantabria", year: 2013, under18age: 0, from18to40age: 1, over40age: 0}];
