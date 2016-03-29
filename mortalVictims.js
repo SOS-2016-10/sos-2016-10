05e5443d40bae5ea3a6d4138dd682795b6340fa4
Pascual Requena Lama <reque1993@gmail.com>
2016-03-29 15:25:35 +0200

    Finishing D02 v1.0. Closes #26

--- mortalVictims.js
+++ mortalVictims.js
@@ -1,6 +1,6 @@
 var express = require("express");
 // URI:/api/v1/mortal-victims
-//My initial Array
+//My initial Array to start the app
 var victims = [{ autonomous_community: "andalucia", year: 2015, under18age: 0, from18to40age: 3, over40age: 7} ,
 { autonomous_community: "madrid", year: 2013, under18age: 0, from18to40age: 5, over40age: 4} ,
 { autonomous_community: "cantabria", year: 2013, under18age: 0, from18to40age: 1, over40age: 0} ];
