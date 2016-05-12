8818ee6065fa8db6fe134e2d47beb4f76e61ac85
Juan Luis Dorante <juan_li24@hotmail.com>
2016-05-12 20:14:13 +0200

    edit pagination

--- static/html/data/divorces-spanish/htmlFunctions.js
+++ static/html/data/divorces-spanish/htmlFunctions.js
@@ -1,3 +1,206 @@
+var contSearch=0;
+var limitOnChange1=0;
+var limitOnChange2=0;
+
+//Funcion NEXT
+function next(){
+  var limitPage = parseInt( $("#limitt").val() );
+  limitOnChange2=limitOnChange1;
+  limitOnChange1=limitPage;
+  //
+  if(limitOnChange1 != limitOnChange2){
+      contSearch=0;
+  }
+  var key=$('#apikey').val();
+  var request = $.ajax({
+      url:"/api/v1/divorces-spanish?apikey="+key+"&offset="+contSearch+"&limit="+limitPage,
+      type:"GET",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        //$("#columns").datagrid("loadData", {"total":data.length,"rows":data});
+        $("#columns").DataTable( {
+          destroy: true,
+          ordering: false,
+          paging: false,
+          searching: false,
+          data: data,
+          "columns": [
+            { data: "autonomous_community" },
+            { data: "year"},
+            { data: "age_0_18"},
+            { data: "age_19_24"},
+            { data: "age_25_29"},
+            { data: "age_30_34"}
+          ]
+        });
+        $("#next").prop( "disabled", false );
+        $("#last").prop( "disabled", false );
+
+        if(limitPage>data.length){
+           $("#next").prop( "disabled", true );
+        }else{
+           contSearch+=limitPage;
+        }
+      },
+      error: function (jqXHR){
+        if(jqXHR.status==401){
+          alert("UNAUTHORIZED, you should change the apikey");
+        }
+      }
+  });
+}
+//Funcion LAST
+function last(){
+  var limitPage=parseInt( $("#limitt").val() );
+  var key=$('#apikey').val();
+  contSearch-=limitPage;
+
+  limitOnChange2=limitOnChange1;
+  limitOnChange1=limitPage;
+  if(limitOnChange1!=limitOnChange2){
+    contSearch=0;
+  }
+
+  var request = $.ajax({
+    url:"/api/v1/divorces-spanish?apikey="+key+"&offset="+contSearch+"&limit="+limitPage,
+    type:"GET",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+      //$('#participantsnumber').datagrid('loadData', {"total":data.length,"rows":data});
+      $("#columns").DataTable( {
+        destroy: true,
+        ordering: false,
+        paging: false,
+        searching: false,
+        data: data,
+        "columns": [
+          { data: "autonomous_community" },
+          { data: "year"},
+          { data: "age_0_18"},
+          { data: "age_19_24"},
+          { data: "age_25_29"},
+          { data: "age_30_34"}
+        ]
+      });
+      $("#next").prop( "disabled", false );
+      if(contSearch<=0){
+        $("#last").prop( "disabled", true );
+        contSearch+=limitPage;
+      }
+    },
+    error: function (jqXHR){
+      if(jqXHR.status==401){
+        alert("UNAUTHORIZED, you should change the apikey");
+      }
+    }
+  });
+}
+
+//Funcion NEXTYEAR
+function nextYear(){
+  var year = parseInt($("#yearS").val());
+  var limitPage = parseInt( $("#limitt").val() );
+  limitOnChange2=limitOnChange1;
+  limitOnChange1=limitPage;
+  //
+  if(limitOnChange1 != limitOnChange2){
+      contSearch=0;
+  }
+  var key=$('#apikey').val();
+  var request = $.ajax({
+      url:"/api/v1/divorces-spanish/?apikey="+key+"&year="+year+"&offset="+contSearch+"&limit="+limitPage,
+      type:"GET",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        //$("#columns").datagrid("loadData", {"total":data.length,"rows":data});
+        $("#columns").DataTable( {
+          destroy: true,
+          ordering: false,
+          paging: false,
+          searching: false,
+          data: data,
+          "columns": [
+            { data: "autonomous_community" },
+            { data: "year"},
+            { data: "age_0_18"},
+            { data: "age_19_24"},
+            { data: "age_25_29"},
+            { data: "age_30_34"}
+          ]
+        });
+        $("#nexty").prop( "disabled", false );
+        $("#lasty").prop( "disabled", false );
+
+        if(limitPage>data.length){
+           $("#nexty").prop( "disabled", true );
+        }else{
+           contSearch+=limitPage;
+        }
+      },
+      error: function (jqXHR){
+        if(jqXHR.status==401){
+          alert("UNAUTHORIZED, you should change the apikey");
+        } else if(jqXHR.status==404){
+          alert("NOT FOUND, please write another YEAR");
+        }
+      }
+  });
+}
+//Funcion LASTYEAR
+function lastYear(){
+  var year = parseInt($("#yearS").val());
+  var limitPage=parseInt( $("#limitt").val() );
+  var key=$('#apikey').val();
+  contSearch-=limitPage;
+
+  limitOnChange2=limitOnChange1;
+  limitOnChange1=limitPage;
+  if(limitOnChange1!=limitOnChange2){
+    contSearch=0;
+  }
+
+  var request = $.ajax({
+    url:"/api/v1/divorces-spanish/?apikey="+key+"&year="+year+"&offset="+contSearch+"&limit="+limitPage,
+    type:"GET",
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+      //$('#participantsnumber').datagrid('loadData', {"total":data.length,"rows":data});
+      $("#columns").DataTable( {
+        destroy: true,
+        ordering: false,
+        paging: false,
+        searching: false,
+        data: data,
+        "columns": [
+          { data: "autonomous_community" },
+          { data: "year"},
+          { data: "age_0_18"},
+          { data: "age_19_24"},
+          { data: "age_25_29"},
+          { data: "age_30_34"}
+        ]
+      });
+      $("#nexty").prop( "disabled", false );
+      if(contSearch<=0){
+        $("#lasty").prop( "disabled", true );
+        contSearch+=limitPage;
+      }
+    },
+    error: function (jqXHR){
+      if(jqXHR.status==401){
+        alert("UNAUTHORIZED, you should change the apikey");
+      } else if(jqXHR.status==404){
+        alert("NOT FOUND, please write another YEAR");
+      }
+    }
+  });
+}
+
+
 //Funcion auxiliar
 //Funcion numeroFilas
 function numRow(){
@@ -8,7 +211,7 @@ function numRow(){
 }
 
 //Funcion paginar
-function paginar(total){
+/*function paginar(total){
   var limit = $("#limitt").val();
   var num = Math.ceil(total/limit); //Redondeo por arriba para paginación
   console.log("HOY SI: "+num);
@@ -18,8 +221,7 @@ function paginar(total){
     //console.log("<input id=\"pg"+i+"\" type=\"button\" value=\""+i+"\" onclick=\"create_table('/api/v1/divorces-spanish/?apikey=juanlur&limit="+limit+"&offset="+cont+"')\" />");
     cont = parseInt(cont) + parseInt(limit);
   }
-
-}
+}*/
 
 //Funcion para crear las tablas
 function create_table(url){
@@ -63,10 +265,10 @@ function mostrarUpdate(){
   document.getElementById('ocultoUpdate').style.display = 'block';
 }
 
-//Funcion mostrar botones NEXT y LAST para paginacion
+/*//Funcion mostrar botones NEXT y LAST para paginacion
 function mostrarLastNext(){
   document.getElementById('paginas').style.display = 'block';
-}
+}*/
 
 //Funcion para validar apikey
 function keyOK(request){
@@ -123,10 +325,24 @@ function searchByYear(){
   var year = $("#yearS").val();
   console.log(year);
   if(year.length == 0){ ///// Search vacío(NO SEARCH) ////////
-    this.create_table('/api/v1/divorces-spanish?apikey=juanlur');
+    //Muestro botones NEXT y LAST ***oculto inversa***
+    $("#last").show();
+    $("#next").show();
+    $("#lasty").hide();
+    $("#nexty").hide();
+
+    var limit = parseInt( $("#limitt").val() );
+    this.create_table('/api/v1/divorces-spanish?apikey=juanlur&limit='+limit+'&offset=0');
   } else { ////////////////// HAY SEARCH //////////////////////
+    //Oculto botones NEXT y LAST ****muestro inversa***
+    $("#last").hide();
+    $("#next").hide();
+    $("#lasty").show();
+    $("#nexty").show();
+
+    var limit = parseInt( $("#limitt").val() );
     var key = $("#apikey").val();
-    var url = "/api/v1/divorces-spanish/?apikey="+key+"&year="+year;
+    var url = "/api/v1/divorces-spanish/?apikey="+key+"&year="+year+"&limit="+limit+"&offset=0";
     console.log(url);
     this.create_table(url);
     } //fin else
