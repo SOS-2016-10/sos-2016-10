029fa481659f2bf8997262ce51c913906c0e1c48
Juan Luis Dorante <juan_li24@hotmail.com>
2016-05-25 17:49:29 +0200

    clean directory of job. Closes #41

---
+++ static/old_versions/v2/index_time_sin_js.js
@@ -0,0 +1,83 @@
+//Ejecución ASINCRONA con EXPRESS
+var express = require("express");
+
+var app = express();
+//Puede tener 2 valores, o la variable entorno PORT o 3000
+//Evaluación perezosa
+var port = (process.env.PORT || 3000);
+
+//Me creo rutas estáticas
+app.use(express.static(__dirname + '/css')); //Es como si folder "css" no existiera, y estuviera todo en RAIZ
+app.use(express.static(__dirname + '/html')); //Es como si folder "html" no existiera, y estuviera todo en RAIZ
+//app.use('/about/divorces-spanish', express.static(__dirname + '/html/about/divorces-spanish')); //lo que quiero que ponga en url, donde está realmente
+
+//GET para página principal
+app.get("/",(req,res) => {
+  console.log("Main Menu");
+});
+
+//GET para ABOUT
+app.get("/about",(req,res) => {
+  console.log("About");
+});
+
+//GET para TIME
+app.get("/time",(req,res) => {
+  var now = new Date();
+  //res.send("It is "+now);
+  res.write("<html><head><link href=\"/style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body>");
+  res.write("<p>It is "+now+"</p>");
+  res.write("</body></html>");
+  res.end();
+  console.log("New request to TIME arrived!");
+});
+
+//GET para DIVORCES-SPANISH(Juanlu)
+app.get("/about/divorces-spanish",(req,res) => {
+  console.log("Divorces-spanish");
+});
+
+
+//GET para Telematic-monitorings(Ulises)
+app.get('/about/telematic-monitorings', (req,res)=>{
+  console.log("Telematic-monitorings");
+  res.write("<html><body><h1>Telematic-monitorings</h1>");
+  res.write("<p>Hello World!, My data is related to the number of telematic monitoring systems in Spain</div><div> I will get the info from this source:http://www.violenciagenero.msssi.gob.es/violenciaEnCifras/home.htm</p>");
+  res.write("</body></html>");
+  res.end();
+});
+
+//GET para Mortal-victims(Requena)
+app.get('/about/mortal-victims', (req,res)=>{
+  console.log("Mortal-victims");
+  res.write("<html><body><h1>Mortal-victims</h1>");
+  res.write("<p>Welcome, My data is related to the number of mortal victims in Spain</div><div> I will get the info from this source:<a href= \" http://www.violenciagenero.msssi.gob.es/violenciaEnCifras/boletines/home.htm \">Here</a></p>");
+  //Tabla
+  //Columnas
+  res.write("<table cellspacing=\"10\" cellpadding=\"10\" border=\"3\"><tr><th scope=\"col\">autonomous-community</th>");
+  res.write("<th scope=\"col\"><strong>year</th>");
+  res.write("<th scope=\"col\"><strong>victim-age</th>");
+  res.write("<th scope=\"col\"><strong>mortal-victims</th></tr>");
+
+  //Filas
+  res.write("<tr><td>Andalucia</td>");
+  res.write("<td>2015</td>");
+  res.write("<td>1-30</td>");
+  res.write("<td>3</td></tr>");
+
+  res.write("<tr><td>Madrid</td>");
+  res.write("<td>2013</td>");
+  res.write("<td>31-85</td>");
+  res.write("<td>9</td></tr></table>");
+
+  res.write("</body></html>");
+  res.end();
+});
+
+
+//app.listen(3000); //Para probar en local
+//app.listen(process.env.PORT); //variable entorno para puerto que me dice Heroku
+//Hago callback porque es asincrono
+app.listen(port, ()=>{
+  console.log("Magic happens on port "+port);
+});
---
+++ static/old_versions/v3/divorcesSpanishFile.js
@@ -0,0 +1,275 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+///Array sobre el que hago las operaciones
+var divorces = [{ autonomous_community: "andalucia", year: 2014, age_0_18: 1, age_19_24: 56, age_25_29: 537, age_30_34: 1860 },
+{ autonomous_community: "aragon", year: 2014, age_0_18: 0, age_19_24: 6, age_25_29: 69, age_30_34: 235 },
+{ autonomous_community: "andalucia", year: 2013, age_0_18: 3, age_19_24: 77, age_25_29: 607, age_30_34: 1956 } ]; //No es JSON(es JavaScript)
+
+//Función para buscar en array "divorces" en mi caso
+function find_resource(array,name){
+  for(var i=0;i<array.length;i++){
+    if(array[i].autonomous_community == name){
+      //console.log(array[i]);
+      var error = 0; //No hay error
+      break;
+    } else {
+      var error = 1; //Hay error
+    }
+  }
+  return [error, i];
+}
+
+//find_community
+function find_community(array,name){
+  var aux = []; //Me creo array vacío
+  for(var i=0;i<array.length;i++){
+    if(array[i].autonomous_community == name){
+      //console.log(array[i]);
+      var error = 0; //No hay error
+      //break;
+      aux.push(array[i]);
+    } //else {
+      //var error = 1; //Hay error
+    //}
+  }
+  if(aux.length == 0){
+    var error = 1; //Hay error
+  }
+  return [error, i, aux];
+}
+
+//find_year
+function find_year(array,year){
+  var aux2 = []; //Me creo array vacío
+  for(var i=0;i<array.length;i++){
+    if(array[i].year == year){
+      var error = 0; //No hay error
+      aux2.push(array[i]);
+    }
+  }
+  if(aux2.length == 0){
+    var error = 1; //Hay error
+  }
+  return [error, i, aux2];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Operaciones sobre lista de recursos ////////////////////
+//module.exports.getDivorces = (req,res)=>{
+router.get("/", (req,res)=>{
+  if(Object.keys(req.query).length == 0){ //No hay busqueda(nº parámetros = 0)
+    console.log("New GET of \"divorces-spanish\"");
+    res.send(divorces);
+  } else { //Hay parámetros de busqueda
+    var aux3 = [];
+    var aux4 = [];
+    //Hago el BUCLE según el nº de parámetros
+    for(var i=0;i<Object.keys(req.query).length;i++){
+      var valor = Object.keys(req.query)[i];
+
+      if(i == 0){ //////////1ª vez que entro en bucle
+        //Recorro array dado
+        for(var j=0;j<divorces.length;j++){
+          var v = req.query[valor]; //obtengo el contenido de la request
+          if(divorces[j][valor] == v){
+            var error = 0; //No hay error
+            aux3.push(divorces[j]);
+          }
+        }
+        if(aux3.length == 0){
+          var error = 1; //Hay error
+        }
+      } else { //////////Resto de veces que entro en bucle(aux3 ya está lleno)
+        for(var j=0;j<aux3.length;j++){
+          var v = req.query[valor]; //obtengo el contenido de la request
+          if(aux3[j][valor] == v){
+            var error = 0; //No hay error
+            aux4.push(aux3[j]);
+          }
+        }
+        aux3 = aux4;
+        if(aux4.length == 0){
+          var error = 1; //Hay error
+        }
+      }
+
+    } //FIN for principal
+    if(aux4.length == 0){//Devuelvo 1 cosa u otra
+      res.send(aux3);
+    } else {
+      res.send(aux4);
+    }
+  }
+});
+//module.exports.postDivorces = (req,res)=>{
+router.post("/", (req,res)=>{
+  var divorce = req.body;
+
+  //comprobar antes que no existe esa "autonomous_community" ya
+  var e = find_resource(divorces,divorce.autonomous_community)[0];
+  if(e == 0){ //No hay error(lo encuentra, ya existe)
+    res.sendStatus(409); //Conflict
+    console.log("NOT POST because \""+divorce.autonomous_community+"\" exist");
+  } else {
+    divorces.push(divorce);
+    console.log("New POST of resource "+divorce.autonomous_community);
+    res.sendStatus(201); //CREATED
+  }
+});
+//NO PERMITIDO
+//module.exports.putDivorces = (req,res)=>{
+router.put("/", (req,res)=>{
+  console.log("PUT NOT ALLOWED");
+  res.sendStatus(405);
+});
+//module.exports.deleteDivorces = (req,res)=>{
+router.delete("/", (req,res)=>{
+  console.log("New DELETE of \"divorces-spanish\"");
+  divorces.splice(0,divorces.length);
+  res.sendStatus(200);
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Operaciones sobre 1 recurso (autonomous_community) ////////////////////
+//module.exports.getDivorce = (req,res)=>{
+router.get("/:autonomous_community", (req,res)=>{
+  var n = req.params.autonomous_community;
+  console.log("New GET of resource "+n);
+
+  var ac = find_community(divorces,n)[2];
+  var e = find_community(divorces,n)[0];
+  //var e = find_resource(divorces,n)[0];
+  //var i = find_resource(divorces,n)[1];
+  if(e == 0){ //Error == 0 "NO hay error"
+    //res.send(divorces[i]);
+    res.send(ac);
+  } else {
+    res.sendStatus(404);
+  }
+});
+//NO PERMITIDO
+//module.exports.postDivorce = (req,res)=>{
+router.post("/:autonomous_community", (req,res)=>{
+  console.log("POST NOT ALLOWED");
+  res.sendStatus(405);
+});
+//module.exports.putDivorce = (req,res)=>{
+router.put("/:autonomous_community", (req,res)=>{
+  var n = req.body.autonomous_community;
+
+  var e = find_resource(divorces,n)[0];
+  var i = find_resource(divorces,n)[1];
+  if(e == 0){ //No hay error(lo encuentra, ya existe)
+    divorces.splice(i, 1); //Elimino objeto
+    divorces.push(req.body); //Añado objeto
+    console.log("New PUT of resource "+n);
+    res.sendStatus(200);
+  } else {
+    console.log("Resource \""+n+"\" NOT exist");
+    res.sendStatus(404);
+  }
+});
+module.exports.deleteDivorce = (req,res)=>{
+  var n = req.params.autonomous_community;
+
+  var e = find_resource(divorces,n)[0];
+  var i = find_resource(divorces,n)[1];
+  if(e == 0){ //Lo encuentro en "divorces"
+    divorces.splice(i, 1); //delete divorces[i];
+    console.log("New DELETE of resource "+n);
+    res.sendStatus(200);
+  } else { //No lo encuentro en "divorces"
+    console.log("Not DELETE because NOT FOUND "+n);
+    res.sendStatus(404);
+  }
+}
+
+//Para inicializar la API REST "divorces-spanish" ///////////////////////////
+// /api/v1/divorces-spanish/loadInitialData“
+module.exports.loadInitialData = (req,res)=>{
+  console.log("/api/v1/divorces-spanish/loadInitialData");
+  divorces.push({ autonomous_community: "canarias", year: 2014, age_0_18: 0, age_19_24: 10, age_25_29: 149, age_30_34: 429 });
+  divorces.push({ autonomous_community: "cantabria", year: 2014, age_0_18: 0, age_19_24: 5, age_25_29: 25, age_30_34: 103 });
+  divorces.push({ autonomous_community: "extremadura", year: 2014, age_0_18: 0, age_19_24: 3, age_25_29: 39, age_30_34: 180 });
+  divorces.push({ autonomous_community: "galicia", year: 2014, age_0_18: 0, age_19_24: 19, age_25_29: 167, age_30_34: 492 });
+  divorces.push({ autonomous_community: "ceuta", year: 2014, age_0_18: 0, age_19_24: 0, age_25_29: 10, age_30_34: 31 });
+  divorces.push({ autonomous_community: "andalucia", year: 2013, age_0_18: 3, age_19_24: 77, age_25_29: 607, age_30_34: 1956 });
+  divorces.push({ autonomous_community: "andalucia", year: 2014, age_0_18: 1, age_19_24: 56, age_25_29: 537, age_30_34: 1860 });
+  //res.send(divorces);
+  res.sendStatus(200);
+}
+
+//////////////////// Operaciones sobre 1 recurso (autonomous_community/year) ////////////////////
+module.exports.getDivorceYear = (req,res)=>{
+  var n = req.params.autonomous_community;
+  var y = req.params.year;
+  console.log("New GET of resource "+n+" "+y);
+
+  var ac = find_community(divorces,n)[2]; //obtengo arrayComunidades
+  var ay = find_year(ac,y)[2]; //obtengo arrayYear de arrayComunidades
+  var e = find_year(ac,y)[0];
+  //var e = find_resource(divorces,y)[0];
+  //var i = find_resource(divorces,n)[1];
+  if(e == 0){ //Error == 0 "NO hay error"
+    //res.send(divorces[i]);
+    res.send(ay);
+  } else {
+    res.sendStatus(404);
+  }
+}
+//NO PERMITIDO
+module.exports.postDivorceYear = (req,res)=>{
+  console.log("POST NOT ALLOWED");
+  res.sendStatus(405);
+}
+module.exports.putDivorceYear = (req,res)=>{
+  //var n = req.body.year;
+
+  var n = req.body.autonomous_community;
+  var y = req.body.year;
+  var ac = find_community(divorces,n)[2]; //obtengo arrayComunidades
+  var ay = find_year(ac,y)[2]; //obtengo arrayYear de arrayComunidades
+  var e = find_year(ac,y)[0];
+  //var e = find_resource(divorces,n)[0];
+  //var i = find_resource(divorces,n)[1];
+  if(e == 0){ //No hay error(lo encuentra, ya existe)
+    divorces.splice(i, 1); //Elimino objeto
+    divorces.push(req.body); //Añado objeto
+    console.log("New PUT of resource "+n+" "+y);
+    res.sendStatus(200);
+  } else {
+    console.log("Resource \""+n+" "+y+"\" NOT exist");
+    res.sendStatus(404);
+  }
+}
+module.exports.deleteDivorceYear = (req,res)=>{
+  var n = req.params.autonomous_community;
+  var y = req.params.year;
+  var ac = find_community(divorces,n)[2]; //obtengo arrayComunidades
+  var ay = find_year(ac,y)[2]; //obtengo arrayYear de arrayComunidades
+  var e = find_year(ac,y)[0];
+
+  //var e = find_resource(divorces,n)[0];
+  //var i = find_resource(divorces,n)[1];
+  if(e == 0){ //Lo encuentro en "divorces"
+    divorces.splice(i, 1); //delete divorces[i];
+    console.log("New DELETE of resource "+n);
+    res.sendStatus(200);
+  } else { //No lo encuentro en "divorces"
+    console.log("Not DELETE because NOT FOUND "+n+" "+y);
+    res.sendStatus(404);
+  }
+}
+
+//OPERACIONES sobre 1 DIVORCE(recurso) ":year"
+module.exports.getYear = (req,res)=>{
+}
+module.exports.postYear = (req,res)=>{
+}
+module.exports.putYear = (req,res)=>{
+}
+module.exports.deleteYear = (req,res)=>{
+}
+module.exports = router;
